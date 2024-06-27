aa5b29a3a6779c41aa13c8e238ecc3622715cbe5
BeratCeylan07 <124052396+BeratCeylan07@users.noreply.github.com>
2024-06-27 20:23:46 +0300

    android sdk update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,10 +9,10 @@ android {
 
     defaultConfig {
         applicationId = "com.bebisocial.bebiapp"
-        minSdk = 29
+        minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
